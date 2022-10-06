12487243320fe74d7508e98c449ee742c9dc8a31
RobX <robxyy@gmail.com>
2022-10-06 18:10:37 +0800

    Add more tests.

--- id/src/commonTest/kotlin/diglol/id/IdTest.kt
+++ id/src/commonTest/kotlin/diglol/id/IdTest.kt
@@ -7,23 +7,34 @@ import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.test.assertFails
-import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class IdTest {
 
   @Test
-  fun generate() {
-    assertContentEquals(Id.generate().machine, Id.generate().machine)
-    assertEquals(Id.generate().pid, Id.generate().pid)
-
+  fun setMachine() {
     assertFails {
       Id.machine = nextBytes(2)
     }
+  }
 
-    val machine = nextBytes(3)
-    Id.machine = machine
-    assertFalse(machine.contentEquals(Id.machine))
+  @Test
+  fun resetMachine() {
+    val machine = Id.machine
+    Id.machine = nextBytes(3)
+    assertContentEquals(machine, Id.machine)
+  }
 
+  @Test
+  fun generate() {
+    val id1 = Id.generate()
+    val id2 = Id.generate()
+    assertEquals(id1.pid, id2.pid)
+    assertContentEquals(id1.machine, id2.machine)
+  }
+
+  @Test
+  fun generateByEpochSeconds() {
     val epochSeconds = epochSeconds()
     val id = Id.generate(epochSeconds)
     assertEquals(epochSeconds, id.time)
@@ -49,5 +60,15 @@ class IdTest {
       0x86.toByte(), 0xe4.toByte(), 0x28, 0x41, 0x2d, 0xc9.toByte()
     )
     assertContentEquals(bytes, id.bytes)
+  }
+
+  @Test
+  fun compareTo() {
+    assertTrue(Id.generate() < Id.generate())
+  }
+
+  @Test
+  fun emptyId() {
+    assertEquals(Id.empty.encodeToString().decodeToId(), Id.empty)
   }
 }
